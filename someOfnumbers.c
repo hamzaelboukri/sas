fc4fc8b9ad615e1554c1e47e640cde7a09306f5d
hamza elboukri <hamzaelboukri01@gmail.com>
2024-09-23 11:04:38 -0700

    add programe that give us some of tow numbers

---
+++ someOfnumbers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main (){
+    int x,j,some;
+         printf("la some\n");
+
+    printf("enter number x :");
+        scanf("%d",&x);
+    printf("enter number j :");
+    
+        scanf("%d",&j);
+        some=x+j;
+        printf("%d + %d = %d",x,j,some);
+
+        return 0;
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+}
